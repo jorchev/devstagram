4faa5b0ffca3871488bc9643c21fc8a13421eae9
Jorge Vega <jorge.hvr@gmail.com>
2022-08-15 18:21:13 -0700

    Creando navegacion y estilos para el layout principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,18 +3,32 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Devstagram - @yield('titulo')</title>
+        <title>DevStagram - @yield('titulo')</title>
         @vite('resources/css/app.css')
     </head>
-    <body>
-        <nav>
-            <a href="/">Principal</a>
-            <a href="/nosotros">Nosotros</a>
-            <a href="/tienda">Tienda</a>
-        </nav>
-        <h1 class="text-3xl font-bold underline text-blue-500">@yield('titulo')</h1>
-        <hr>
-        @yield('contenido')
+    <body class="bg-gray-100">
+
+        <header class="p-5 border-b bg-white shadow">
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">DevStagram</h1>
+                <nav class="flex gap-2 items-center">
+                    <a href="#" class="font-bold uppercase text-gray-600 text-sm">Login</a>
+                    <a href="#" class="font-bold uppercase text-gray-600 text-sm">Crear Cuenta</a>
+                </nav>
+            </div>
+            
+        </header>
+
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>
+            @yield('contenido')
+        </main>
+
+        <footer class="text-center p-5 text-gray-500 font-bold">
+            DevStagram - Todos los derechos reservados {{ now()->year }}
+        </footer>
 
     </body>
 </html>
