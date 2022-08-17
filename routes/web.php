7afd53c565df73b43335b1d7ab3df0993d30480f
Jorge Vega <jorge.hvr@gmail.com>
2022-08-16 18:56:03 -0700

    Creando un controlador para las vistas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('principal');
 });
 
-Route::get('/crear-cuenta', function () {
-    return view('auth.register');
-});
+Route::get('/crear-cuenta', [RegisterController::class,'index']);
