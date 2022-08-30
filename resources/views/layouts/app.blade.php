97e5390c9c6342e9fca7b811cb6a1f514e515f19
Jorge Vega <jorge.hvr@gmail.com>
2022-08-30 00:57:49 -0700

    Creando el formulario de registro en Devstagram

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
             @yield('contenido')
         </main>
 
-        <footer class="text-center p-5 text-gray-500 font-bold">
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold">
             DevStagram - Todos los derechos reservados {{ now()->year }}
         </footer>
 
