f5d546b6b98b5ffbaec68713ebec97db13ce1eae
Jorge Vega <jorge.hvr@gmail.com>
2022-09-05 18:25:50 -0700

    Revisar con el password confirmado

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,7 +43,7 @@
 
                 <div class="mb-5">
                     <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Password de registro" class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror"  value={{ old('password') }}>
+                    <input type="password" name="password" id="password" placeholder="Password de registro" class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror">
                 
                     @error('password')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
