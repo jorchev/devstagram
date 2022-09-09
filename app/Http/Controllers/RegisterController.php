d990a772f2f4b4ecde215cf51ea4316d04ae32b5
Jorge Vega <jorge.hvr@gmail.com>
2022-09-08 19:18:27 -0700

    Redireccionar al usuario al muro una vez su cuenta es creada

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -36,5 +36,8 @@ class RegisterController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
+        
+
+        return redirect()->route('posts.index');
     }
 }
